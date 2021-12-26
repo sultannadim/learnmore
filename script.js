8daaab437b14cf974226b957fc92e9b0a5e64a0f
sultan nadim <sultan5218@gmail.com>
2021-12-26 16:57:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let more = document.querySelectorAll('.more');
-for (let i = 0; i<more.length; i++){
-	more[i].addEventListener('click', function(){
-		more[i].parentNode.classList.toggle('active')
-	})
-}
+// let more = document.querySelectorAll('.more');
+// for (let i = 0; i<more.length; i++){
+// 	more[i].addEventListener('click', function(){
+// 		more[i].parentNode.classList.toggle('active')
+// 	})
+// }
+
+$(document).ready(function(){
+	$('.more').on('click', function(){
+		$(this).parent().toggleClass('active');
+	});
+});
